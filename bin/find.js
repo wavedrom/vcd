08144a4d7b9e101fdc454a6076de4bdbb0f4af50
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-11-06 18:35:15 -0800

    streaming version of parser

--- bin/find.js
+++ bin/find.js
@@ -10,7 +10,7 @@ const dir = './tmp/';
 
 fs.readdir(dir).then(files => {
   const tt0 = Date.now();
-  async.eachLimit(files, 1, (fileName, callback) => {
+  async.eachLimit(files, 2, (fileName, callback) => {
 
     let start = 0;
     let stop = 0;
@@ -18,29 +18,31 @@ fs.readdir(dir).then(files => {
 
     let inst = vcd();
 
-    // inst.on('$enddefinitions', () => {
+    inst.on('$enddefinitions', () => {
       // console.log(res);
       // console.log(inst.info);
-    // });
-
-    inst.onTrigger('D1', time => {
-      if (time > 10) {
-        if (start == 0) {
-          start = time;
-        } else {
-          stop = time;
+      inst.onTrigger('D1', time => {
+        if (time > 10) {
+          if (start == 0) {
+            start = time;
+          } else {
+            stop = time;
+          }
         }
-      }
+      });
     });
 
-    const s = fs.createReadStream(dir + fileName);
-
-    s.on('data', inst.execute);
-
-    s.on('end', () => {
+    inst.on('finish', () => {
       console.log(fileName, (stop - start), ((Date.now() - t0) / 1000 + 's'));
       callback();
     });
+
+    inst.on('error', err => {
+      console.log(err);
+    });
+
+    fs.createReadStream(dir + fileName).pipe(inst);
+
   }, () => {
     console.log('Total time: ' + (Date.now() - tt0) / 1000 + 's');
   });
--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 'use strict';
 
+const stream = require('stream');
 const EventEmitter = require('events').EventEmitter;
 const pkg = require('./package.json');
 let lib = require('bindings')('vcd.node');
 
-const version = pkg.version;
-
 module.exports = () => {
-  const lifee = new EventEmitter();
-  const triee = new EventEmitter();
-
-  const lifemit = lifee.emit.bind(lifee);
-  const triemit = triee.emit.bind(triee);
 
   const info = {path: []};
 
+  const s = new stream.Writable();
+
+  // const lifee = new EventEmitter();
+  const lifemit = s.emit.bind(s);
+
+  const triee = new EventEmitter();
+  const triemit = triee.emit.bind(triee);
+
   const cxt = lib.init(lifemit, triemit, info);
 
-  const execute = chunk => lib.execute(cxt, lifemit, triemit, info, chunk);
-
-  const on = (id, fn) => {
-    lifee.on(id, fn);
+  s._write = function (chunk, encoding, callback) {
+    lib.execute(cxt, lifemit, triemit, info, chunk);
+    callback();
   };
 
-  const onTrigger = (id, fn) => {
+  s.onTrigger = (id, fn) => {
     lib.setTrigger(cxt, id);
     triee.on(id, fn);
   };
 
-  return {
-    version,
-    info,
-    execute,
-    on,
-    onTrigger
-  };
+  s.version = pkg.version;
+
+  s.info = info;
+
+  return s;
 };
