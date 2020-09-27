351f988fb7e009adb2730aa216437303ac3fec6f
Ben <bjmorse@gmail.com>
2020-09-27 02:15:35 -0700

    rework sigsg

--- wasm_main.cpp
+++ wasm_main.cpp
@@ -34,33 +34,49 @@ typedef void externalJsMethodOne(const int sz);
 typedef void externalJsMethodTwo(const char*, const uint64_t time, const uint8_t command, const int dnc0, const int dnc1);
 
 
-typedef int  externalJsGetProperty(const char* name);
+typedef int  externalJsGetProperty(const char* name, const size_t len);
 typedef void externalJsSetProperty(const char* name, const size_t len, const int value);
 
 
 /// function pointer for c->js
 static externalJsMethodOne* externalOne = 0;
 static externalJsMethodTwo* externalTwo = 0;
-static externalJsSetProperty* set_property = 0;
-static externalJsGetProperty* get_property = 0;
+static externalJsSetProperty* bound_set_property = 0;
+static externalJsGetProperty* bound_get_property = 0;
+
+static void set_property(const char* name, const int value) {
+  bound_set_property(name, strlen(name), value);
+}
+
+static int get_property(const char* name) {
+  return bound_get_property(name, strlen(name));
+}
 
 
 
 extern "C" {
 
 void execute(
+  const int context,
   externalJsMethodOne* f1,
   externalJsMethodTwo* f2,
   externalJsSetProperty* sfn,
-  externalJsGetProperty* gfn
+  externalJsGetProperty* gfn,
+  char* chunk
   ) {
 
   // cout << "execute got " << p << "\n";
-  cout << "execute " << (int)sfn << "\n";
-  set_property = sfn;
+  cout << "execute " << (int)sfn << " and got " << chunk << "\n";
+  bound_set_property = sfn;
+  bound_get_property = gfn;
 
-  set_property("foo", strlen("foo"), 4);
   
+
+  int got = get_property("bar");
+
+  cout << "got " << got << " for bar\n";
+  
+
 
 }
 
