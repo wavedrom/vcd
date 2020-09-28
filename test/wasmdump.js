ae8f76a0c012c4e6dcdea5b5c067c09a0e59964e
Ben <bjmorse@gmail.com>
2020-09-27 19:05:27 -0700

    fix warning

--- test/wasmdump.js
+++ test/wasmdump.js
@@ -21,7 +21,6 @@ describe('dump', () => {
     ['"}G', '{u', 'u)'] // array of all signal ids
       .map(id =>
         inst.change.on(id, (time, cmd, value, mask) => {
-          console.log(time, cmd, value, mask);
           dump.push({
             id,
             time,
@@ -82,7 +81,11 @@ $timescale   1ns $end
 0"}G
 #200
 1"}G
-bzzzzxxxx11110000ZZZZXXXX11110000zzzzxxxx11110000zzzzxxxx11110000 {u
+bzzzzxxxx11110000ZZZZXXXX11110000zzzzxxx`);
+
+// break in the middle of the number scan
+
+     inst.write(                       `x11110000zzzzxxxx11110000 {u
 #300
 0"}G
 b1111000000000000000000000000000000000000000000000000000000000000 {u
